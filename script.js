f4fa5d6d943062eedf25e9e0ff94ebc4c0e08160
Ryan10od <165436921+Ryan10od@users.noreply.github.com>
2024-08-09 12:38:28 +0100

    Add files via upload
    First Commit testing out API

---
+++ script.js
@@ -0,0 +1,33 @@
+const searchForm = document.querySelector('form');
+const searchInput = document.querySelector('#search');
+const resultsList = document.querySelector('#results');
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchRecipes();
+})
+
+async function searchRecipes() {
+    const searchValue = searchInput.value.trim();
+    const response = await fetch(`https://api.edamam.com/search?q=${searchValue}&app_id=1efa5979&app_key=
+158fc1181d3732f575dc152e5a232868`);
+    const data = await response.json();
+    displayRecipes(data.hits);
+}
+
+function displayRecipes(recipes) {
+    let html = '';
+    recipes.forEach((recipe) => {
+        html += `
+        <div>
+            <img src="${recipe.recipe.image}" alt="${recipe.recipe.label}">
+            <h3>${recipe.recipe.label}</h3>
+            <ul>
+                ${recipe.recipe.ingredientLines.map(ingredient => `<li>${ingredient}</li>`).join('')}
+            </ul>
+            <a href="${recipe.recipe.url}" target="_blank">View Recipe</a>
+        </div> 
+        `
+    })
+    resultsList.innerHTML = html;
+}
